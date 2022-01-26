831e4ed608719ee57a3f1266a91cc4ce3624a9f6
HongSeingmin <globalsh1@gmail.com>
2022-01-26 13:56:37 +0900

    커밋 테스트_파이썬 IDEA

---
+++ python/baekjoon/test.py
@@ -0,0 +1,14 @@
+'''
+author : Hong Seungmin
+github : https://github.com/HongSeumgMin
+email  : globalsh1@gmail.com
+
+==Baekjoon Online Judge==
+  title : <<title>>
+  link  : <<link>>
+'''
+
+#####
+
+
+print("test")
